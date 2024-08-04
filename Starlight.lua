2d4280b3f6a963e4a487e5bf7dc43d2736caf91c
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2024-08-04 23:22:58 +0800

    Update Starlight.lua

--- Starlight.lua
+++ Starlight.lua
@@ -7,20 +7,22 @@ NG.CFrame = script.Parent.CFrame
 NG.MaxTorque = Vector3.new(399999993722699776, 399999993722699776, 399999993722699776)
 NT.Force = Vector3.new()
 
-local TgtHeight = 1000000
+local orbit_alt = 400000
 local MaxHeight = -10000
 
 local cort = coroutine.create(function() 
-    while script.Parent.Position.Y < MaxHeight or script.Parent.Position.Y < 10000 do
-	MaxHeight = script.Parent.Position.Y
-        NG.CFrame = CFrame.fromEulerAnglesXYZ(0,0,math.pi/2+(((math.pi)*math.clamp(alt,-1000,orbit_alt))/(2*orbit_alt)))
-    end
-    local NV = Instance.new("BodyVelocity")
-    NV.Parent = script.Parent
-    NV.MaxForce = Vector3.new(0, 3099999993722699776, 0)
-    NV.Velocity = Vector3.new(0,0,0)
+	while script.Parent.Position.Y < MaxHeight or script.Parent.Position.Y < 10000 do
+		MaxHeight = script.Parent.Position.Y
+        	NG.CFrame = CFrame.fromEulerAnglesXYZ(0,0,math.pi/2+(((math.pi)*math.clamp(alt,-1000,orbit_alt))/(2*orbit_alt)))
+		wait(0.1)
+	end
+	script.Parent.S3:Destroy()
+	local NV = Instance.new("BodyVelocity")
+	NV.Parent = script.Parent
+	NV.MaxForce = Vector3.new(0, 3099999993722699776, 0)
+	NV.Velocity = Vector3.new(0,0,0)
+	print("Gyro Finished")
 end)
-
 
 function TogPlm(model,bool)
 	for i,v in ipairs(model:GetDescendants()) do
@@ -39,22 +41,24 @@ function TogPlm(model,bool)
 	end
 end
 
-TogPlm(script.Parent.Parent.S1.Model1.eModel,false)
-TogPlm(script.Parent.Parent.S2,false)
+if script.Parent.Parent.S1 then
+	TogPlm(script.Parent.Parent.S1.Model1.eModel,true)
+	wait(5)
+	script.Parent.S0:Destroy()
+	coroutine.resume(cort)
+	NT.Force = Vector3.new(0, 0, -20000000)
+	wait(80)
+	TogPlm(script.Parent.Parent.S1.Model1.eModel,false)
+	NT.Force = Vector3.new(0, 0, -15000000)
+	script.Parent.S1:Destroy()
+	wait(1)
+end
+if script.Parent.Parent.S2 then
+	TogPlm(script.Parent.Parent.S2,true)
+	wait(120)
+	NT.Force = Vector3.new(0, 0, 0)
+	TogPlm(script.Parent.Parent.S2,false)
+end
+print("Script Finished")
 
-TogPlm(script.Parent.Parent.S1.Model1.eModel,true)
-wait(5)
-script.Parent.S0:Destroy()
-coroutine.resume(cort)
-NT.Force = Vector3.new(0, 0, -20000000)
-wait(80)
-TogPlm(script.Parent.Parent.S1.Model1.eModel,false)
-NT.Force = Vector3.new(0, 0, -15000000)
-script.Parent.S1:Destroy()
-wait(1)
-TogPlm(script.Parent.Parent.S2,true)
-wait(20)
-script.Parent.S3:Destroy()
-wait(100)
-NT.Force = Vector3.new(0, 0, 0)
-TogPlm(script.Parent.Parent.S2,false)
+
