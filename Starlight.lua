5608159f5720f637319d9bd397a7ee9103a55ee0
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2024-08-04 18:29:05 +0800

    Update Starlight.lua

--- Starlight.lua
+++ Starlight.lua
@@ -10,7 +10,6 @@ local TgtHeight = 1000000
 local MaxHeight = -10000
 
 local cort = coroutine.create(function() 
-    NG.MaxTorque = Vector3.new(399999993722699776, 399999993722699776, 399999993722699776)
     while script.Parent.Position < MaxHeight or script.Parent.Position < 10000 do
         NG.CFrame = CFrame.fromEulerAnglesXYZ(0,0,math.pi/2+(((math.pi)*math.clamp(alt,-1000,orbit_alt))/(2*orbit_alt)))
     end
@@ -33,6 +32,7 @@ TogPlm(script.Parent.Parent.S1.Model1.eModel,True)
 wait(5)
 script.Parent.S0:Destroy()
 coroutine.resume(cort)
+NG.MaxTorque = Vector3.new(399999993722699776, 399999993722699776, 399999993722699776)
 NT.Force = Vector3.new(0, 0, -20000000)
 wait(80)
 TogPlm(script.Parent.Parent.S1.Model1.eModel,False)
