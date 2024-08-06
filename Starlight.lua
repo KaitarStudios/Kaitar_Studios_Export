8a13907e8b4bcd5f5cf7d72ac6e1a029e7b6d23a
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2024-08-06 17:41:38 +0800

    Update Starlight.lua

--- Starlight.lua
+++ Starlight.lua
@@ -12,10 +12,11 @@ local MaxHeight = -10000
 
 local cort = coroutine.create(function() 
 	local alt = script.Parent.Position.Y
-	while alt > MaxHeight or alt < 10000 do
+	while alt > MaxHeight-100 or alt < 10000 do
 		alt = script.Parent.Position.Y
 		MaxHeight = alt
-        	NG.CFrame = CFrame.fromEulerAnglesXYZ(math.pi/2+(((math.pi)*math.clamp(alt,-1000,orbit_alt))/(2*orbit_alt)),0,0)
+		print(MaxHeight)
+  NG.CFrame = CFrame.fromEulerAnglesXYZ(math.pi/2+(((math.pi)*math.clamp(alt,-1000,orbit_alt))/(2*orbit_alt)),0,0)
 		wait(0.1)
 	end
 	if script.Parent.Parent.S3 then
