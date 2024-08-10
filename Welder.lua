73093432d618b9850f0ba189edc8d5c97e133af7
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2024-08-10 20:29:24 +0800

    Update Welder.lua

--- Welder.lua
+++ Welder.lua
@@ -3,9 +3,10 @@ local Largepart = nil
 local welds = {}
 for i,v in ipairs(script.Parent:GetDescendants()) do
 	if v:IsA("BasePart") or v:IsA("MeshPart") or v:IsA("UnionOperation") or v:IsA("TrussPart") then
-		if v:FindFirstChildWhichIsA("Attachment") or v:FindFirstChildWhichIsA("WeldConstraint") then
+		if ( v:FindFirstChildWhichIsA("Attachment") or v:FindFirstChildWhichIsA("WeldConstraint") ) and not v.Anchored then
 			break
 		end
+		v.Anchored = False
 		if not Firstpart then
 			Firstpart = v
 		end
@@ -19,6 +20,7 @@ for i,v in ipairs(script.Parent:GetDescendants()) do
 		local NW = Instance.new("WeldConstraint")
 		NW.Parent = v
 		NW.Part0 = v
+		NW.Part1 = Firstpart
 		table.insert(welds,NW)
 	end
 end
