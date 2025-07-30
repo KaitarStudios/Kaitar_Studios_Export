cd529bb6eec3b2b5d976ca19dcd43c3d9b84fc1d
KaitarStudios <143938878+KaitarStudios@users.noreply.github.com>
2025-07-30 20:56:33 +0800

    Update RocketKitServer30.lua

--- RocketKitServer30.lua
+++ RocketKitServer30.lua
@@ -9,7 +9,7 @@ local G0 = 9.81/0.28
 --------------------------------------------
 local EnabledEngines = {}
 local StageList = {}
-print("RD-3.5")
+print("RD-3.6")
 --------------------------------------------
 local TS = game:GetService("TweenService")
 local TO = TweenInfo.new(1,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut,0,false,0)
